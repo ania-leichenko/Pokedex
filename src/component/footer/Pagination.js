dcd3d28bc46c2a382befa61f67aa38031763a213
Anna Leichenko <ania.leichenko@gmail.com>
2021-09-30 19:28:24 +0300

    set up pagination

--- src/component/footer/Pagination.js
+++ src/component/footer/Pagination.js
@@ -12,12 +12,23 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-export default function PaginationOutlined() {
+export default function PaginationOutlined({setCurrentPage, currentPage}) {
   const classes = useStyles();
+
+  function handleClick(event, page) {
+    setCurrentPage(page);
+  }
+
 
   return (
     <Stack spacing={2} className={classes.pagination}>
-      <Pagination count={10} variant="outlined" color="primary" />
+      <Pagination 
+       count={10}
+       page={currentPage}
+       variant="outlined" 
+       color="primary" 
+       onChange={handleClick}
+      />
     </Stack>
   );
 }
